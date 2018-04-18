57fe3ed58739e8b6713ca14725b2e4859e273b47
Rodney Gee <Rodney.Gee@momentum.co.za>
2018-04-18 10:04:56 +0200

    Send messages to only people in same toom

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,9 +1,9 @@
-var moment = require ('moment');
+
 
 var generateMessage = (from, text) => {
   return {from,
           text,
-           createdAt: moment().valueOf()
+           createdAt: new Date().getTime ()
          };
 }
 
@@ -11,7 +11,7 @@ var generateLocationMessage = (from, latitude, longitude) => {
   return {
       from,
       url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-      createdAt: moment().valueOf()
+      createdAt: new Date().getTime ()
          };
 }
 module.exports = {generateMessage, generateLocationMessage};
